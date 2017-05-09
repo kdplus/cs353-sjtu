ceb1247c87db5a4c53f6f0bcee8130f9fd65d569
kdplus <15058617922@163.com>
2017-05-09 21:06:23 +0800

    add mem limit

--- 3/process.c
+++ 3/process.c
@@ -1,3 +1,4 @@
+#include <cstring>
 #include <stdlib.h>
 #include<stdio.h>
 #include <iostream>
@@ -11,6 +12,8 @@ int main(int argc, char *argv[]){
     clock_t starttime;
     int cpu = atoi(argv[1]);
     int mem = atoi(argv[2]);
+    long long* large_mem = new long long[mem * 100000];
+    for (int i = 0; i < mem*100000; ++i) large_mem[i] = i;
     while(1){
         starttime = clock();
         while((clock()-starttime) <= (double)busytime * CLOCKS_PER_SEC / 50 * cpu )
--- 3/schedule.py
+++ 3/schedule.py
@@ -21,7 +21,7 @@ class Sched:
     child_pool = Queue.Queue()
     cpu_sum = 0
     mem_sum = 0
-    c = 0.3
+    c = 3.0
 
     def __init__(self, wq):
         print ("Create a process queue.")
@@ -53,17 +53,30 @@ class Sched:
         self.mem_sum -= p.mem
 
 
+def get_cpu():
+    load = os.popen("cat /proc/loadavg")
+    loadavg = load.read()
+    return loadavg.split(' ')[0]
+
+def get_mem():
+    free = os.popen("free -m")
+    result = free.read().split('\n')[1].split()[2]
+    return result
+
+
 wq = Queue.Queue()
 # create some processes
-for i in range(0,10):
-    a = Process(i, random.random()/10, random.random()/10)
+for i in range(0,20):
+    a = Process(i, random.random(), random.random())
     wq.put(a)
 
 s = Sched(wq);
 
 while True:
+    cpu_status = get_cpu()
+    mem_status = get_mem()
     print ("Status of Schedule:")
-    print ("Cpu:", s.cpu_sum, " Mem: ", s.mem_sum)
+    print ("Cpu:", cpu_status, " Mem: ", mem_status)
     print ("running process queue ----------------------")
 
     for i in range(0, s.pq.qsize()):
@@ -79,13 +92,14 @@ while True:
         s.wq.put(p)
     print (" ")
 
-
-    if s.cpu_sum < s.c and s.mem_sum < s.c:
+    print "Schedule decision:"
+    if float(cpu_status) < s.c and int(mem_status) < 1700:
         s.run()
     else:
-        s.drop()
+        if not s.pq.empty():
+            s.drop()
 
-    time.sleep(8)
+    time.sleep(5)
 
     print " "
     print " "
